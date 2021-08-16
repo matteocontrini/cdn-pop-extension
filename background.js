81914dbff1da837ed5070296d4bd53a8cc169de7
Matteo Contrini <matteo@contrini.it>
2021-08-16 10:07:00 +0200

    Initial version

---
+++ background.js
@@ -0,0 +1,170 @@
+function getHeader(request, needle) {
+    for (let header of request.responseHeaders) {
+        if (header.name.toLowerCase() == needle) {
+            return header.value;
+        }
+    }
+}
+
+function toLower(thing) {
+    if (typeof thing === 'string') {
+        return thing.toLowerCase();
+    }
+    else {
+        return thing;
+    }
+}
+
+let detectors = [
+    {
+        cdn: 'cloudflare',
+        detect: (request) => {
+            let header = getHeader(request, 'cf-ray');
+            if (header) {
+                const pop = header.slice(-3);
+                return { pop };
+            }
+        }
+    },
+    {
+        cdn: 'cloudfront',
+        detect: (request) => {
+            let header = getHeader(request, 'x-amz-cf-pop');
+            if (header) {
+                const pop = header.slice(0, 3);
+                return { pop };
+            }
+        }
+    },
+    {
+        cdn: 'stackpath',
+        detect: (request) => {
+            let header = getHeader(request, 'x-hw');
+            if (header) {
+                const pops = header.split(',');
+                const lastPop = pops[pops.length - 1];
+                const parts = lastPop.split('.');
+                const pop = parts[parts.length - 2];
+                return { pop };
+            }
+        }
+    },
+    {
+        cdn: 'bunny',
+        detect: (request) => {
+            let header = getHeader(request, 'server');
+            if (header && header.indexOf('BunnyCDN') == 0) {
+                const pop = header.split('-')[1];
+                return { pop };
+            }
+        }
+    },
+    {
+        cdn: 'cdn77',
+        detect: (request) => {
+            let pop = getHeader(request, 'x-77-pop');
+            if (pop) {
+                return { pop };
+            }
+        }
+    },
+    {
+        cdn: 'fastly',
+        detect: (request) => {
+            let server = toLower(getHeader(request, 'server'));
+            let timer = getHeader(request, 'x-timer');
+            let vary = getHeader(request, 'vary');
+            if (server == 'artisanal bits' || /^s\d+\.\d+,vs0,ve\d+$/i.test(timer) || /fastly-ssl/i.test(vary)) {
+                let pop = getHeader(request, 'x-served-by').slice(-3);
+                return { pop };
+            }
+        }
+    }
+];
+
+let cnameDetectors = [
+    {
+        cdn: 'akamai',
+        domains: ['akamaiedge.net', 'akamai.net']
+    }
+];
+
+function onRequest(details) {
+    console.log(details);
+    const tabId = details.tabId;
+    if (tabId == -1) {
+        return;
+    }
+
+    let cdn;
+    let pop;
+
+    for (const detector of detectors) {
+        let match = detector.detect(details);
+        if (match) {
+            cdn = detector.cdn;
+            pop = match.pop;
+        }
+    }
+
+    let hostname = new URL(details.url).hostname;
+
+    if (cdn) {
+        updateBadge(tabId, hostname, cdn, pop);
+    }
+    else {
+        browser.dns.resolve(hostname, ['canonical_name']).then(resp => {
+            let canonicalName = resp['canonicalName'];
+            console.log(`Resolved ${hostname} to CNAME ${canonicalName}`);
+
+            for (const detector of cnameDetectors) {
+                for (const domain of detector.domains) {
+                    if (canonicalName.endsWith(domain)) {
+                        updateBadge(tabId, hostname, detector.cdn, null);
+                        return;
+                    }
+                }
+            }
+        });
+    }
+}
+
+function updateBadge(tabId, hostname, cdn, pop) {
+    console.log(tabId, hostname, cdn, pop)
+
+    browser.browserAction.setIcon({
+        path: `icons/${cdn}.png`,
+        tabId: tabId,
+    });
+
+    if (pop) {
+        pop = pop.toUpperCase();
+
+        browser.browserAction.setBadgeText({
+            text: pop,
+            tabId: tabId,
+        });
+    }
+
+    browser.browserAction.setTitle({
+        title: `${cdn} (${pop})`,
+        tabId: tabId,
+    });
+}
+
+browser.webRequest.onCompleted.addListener(
+    onRequest, 
+    {
+        urls: ['<all_urls>'],
+        types: ['main_frame']
+    },
+    ['responseHeaders']
+);
+
+browser.browserAction.setBadgeBackgroundColor({
+    color: 'rgba(243, 128, 32, 0.5)',
+});
+
+browser.browserAction.setBadgeTextColor({
+    color: '#fff',
+});
